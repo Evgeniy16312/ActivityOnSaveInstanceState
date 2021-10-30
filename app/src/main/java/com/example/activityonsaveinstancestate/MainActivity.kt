de3f260eb10e434dfed735eb7cde349369fc07a1
Evgeniy16312 <84197411+Evgeniy16312@users.noreply.github.com>
2021-10-30 19:40:41 +0400

    Initial commit

---
+++ app/src/main/java/com/example/activityonsaveinstancestate/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.activityonsaveinstancestate
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.activityonsaveinstancestate.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var state: State
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.incrementButton.setOnClickListener { increment() }
+        binding.randomColorButton.setOnClickListener { setRandomColor() }
+        binding.switchVisibilityButton.setOnClickListener { switchVisibility() }
+
+        state = if (savedInstanceState == null) {
+            State(
+                counterValue = 0,
+                counterTextColor = ContextCompat.getColor(this, R.color.purple_700),
+                counterIsVisible = true
+            )
+        } else {
+            savedInstanceState.getParcelable(KEY_STATE)!!
+        }
+        renderState()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_STATE, state)
+    }
+
+    private fun increment() {
+        state.counterValue++
+        renderState()
+    }
+
+    private fun setRandomColor() {
+        state.counterTextColor = Color.rgb(
+            Random.nextInt(256),
+            Random.nextInt(256),
+            Random.nextInt(256)
+        )
+        renderState()
+    }
+
+    private fun switchVisibility() {
+        state.counterIsVisible = !state.counterIsVisible
+        renderState()
+    }
+
+    private fun renderState() = with(binding) {
+        counterTextView.setText(state.counterValue.toString())
+        counterTextView.setTextColor(state.counterTextColor)
+        counterTextView.visibility = if (state.counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+
+    class State(
+        var counterValue: Int,
+        var counterTextColor: Int,
+        var counterIsVisible: Boolean
+    ) : Parcelable {
+        constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readByte() != 0.toByte()
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeInt(counterValue)
+            parcel.writeInt(counterTextColor)
+            parcel.writeByte(if (counterIsVisible) 1 else 0)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<State> {
+            override fun createFromParcel(parcel: Parcel): State {
+                return State(parcel)
+            }
+
+            override fun newArray(size: Int): Array<State?> {
+                return arrayOfNulls(size)
+            }
+        }
+
+    }
+
+    companion object {
+        @JvmStatic
+        private val KEY_STATE = "STATE"
+    }
+}
